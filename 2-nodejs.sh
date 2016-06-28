64164fc91932d37a6971c2e3ddcd24f183c0b2e9
chenhf <4019980@qq.com>
2016-06-28 16:28:59 +0800

    update

--- 2-nodejs.sh
+++ 2-nodejs.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 nvm install v6.2.1
+
+# or https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-a-centos-7-server
+wget http://nodejs.org/dist/v0.10.30/node-v0.10.30.tar.gz
+tar xzvf node-v* && cd node-v*
+sudo yum install gcc gcc-c++
+./configure
+make
+sudo make install
